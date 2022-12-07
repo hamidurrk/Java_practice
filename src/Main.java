a4e976eb6c227bae9676eecebc79e6375ab27a9f
hamidurrk <hamidurrk@gmail.com>
2022-12-07 12:16:54 +0600

    practicing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("Madlibs!");
+
+        String username = "magicPony420!";
+        System.out.println("Hello ___ helloo ____ helloooo _____");
+        String person1 = "atik";
+        String person2 = "abella";
+        String person3 = "antu";
+        System.out.println("Hello " + person1);
+        System.out.println("He \nsaid, \"I don't\tknow about \nanything else\";");
     }
 }
